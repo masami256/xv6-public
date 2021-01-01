4a29a2864e07dd89bfd5d9b73713efe8b4770fcf
Masami Ichikawa <masami256@gmail.com>
2021-01-01 10:53:49 +0900

    virtio: make pci device data global in the file

--- virtio.c
+++ virtio.c
@@ -1,18 +1,19 @@
 #include "virtio.h"
 
+struct pci_device_info virtio_pci_dev;
+
 void virtio_init(void)
 {
-	struct pci_device_info info;
 	int r;
 
-	memset(&info, 0, sizeof(info));
 	cprintf("initialize virtio\n");
+	memset(&virtio_pci_dev, 0, sizeof(virtio_pci_dev));
 
-	r = search_pci_device(VIRTIO_VENDOR, &info);
+	r = search_pci_device(VIRTIO_VENDOR, &virtio_pci_dev);
 	if (r) 
 		panic("virtio device is not found\n");
 			
 
-	cprintf("[+]found virtio device(vendor:0x%x) at %d:%d\n", info.device.vendor, info.device.bus, info.device.devfn);
+	cprintf("[+]found virtio device(vendor:0x%x) at %d:%d\n", virtio_pci_dev.device.vendor, virtio_pci_dev.device.bus, virtio_pci_dev.device.devfn);
 }
 
