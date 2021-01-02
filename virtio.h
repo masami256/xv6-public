a7e806558fa3db4ac4acc6afe207cdf36c92fbcc
Masami Ichikawa <masami256@gmail.com>
2021-01-02 23:02:44 +0900

    virtio: add definisions and spec title

--- virtio.h
+++ virtio.h
@@ -5,13 +5,22 @@
 #include "defs.h"
 #include "pci.h"
 
+// spec 4.1.2 PCI Device Discovery https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-1020002
 #define VIRTIO_VENDOR 0x1AF4
 
-// https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-1020002
+// spec 4.1.2.1 Device Requirements: PCI Device Discovery  https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-1020002
 #define VIRTIO_PCI_DEVICE_BLOCK_DEVICE 0x1001
 
-// https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-1930005
+// spec 5 Device Types https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-1930005
 #define VIRTIO_DEVICE_TYPE_BLOCK_DEVICE 0x02
+
+// spec 2.1 Device Status Field https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-100001
+#define VIRTIO_STATUS_ACKNOWLEDGE 1
+#define VIRTIO_STATUS_DRIVER 2
+#define VIRTIO_STATUS_FAILD 128
+#define VIRTIO_STATUS_FEATURES_OK 8
+#define VIRTIO_DRIVER_OK 4
+#define VIRTIO_DEVICE_NEEDS_RESET 64
 
 void virtio_init(void);
 
