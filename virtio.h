add62156c05cc6b8b5b561b298ef43a4d364be40
Masami Ichikawa <masami256@gmail.com>
2021-01-06 00:18:26 +0900

    virtio: add virtqueues structures.

--- virtio.h
+++ virtio.h
@@ -47,6 +47,7 @@
 #define VIRTIO_BLK_F_BARRIER		0
 #define VIRTIO_BLK_F_SCSI			7
 
+
 // spec 1.4 Structure Specifications https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-80004
 typedef short le16;
 typedef int le32;
@@ -58,6 +59,48 @@ typedef long long be64;
 
 struct virtio_device_info {
 	struct pci_device pci;
+};
+
+// spec 2.6.5 The Virtqueue Descriptor Table https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-320005
+#define VIRTQ_DESC_F_NEXT		1
+#define VIRTQ_DESC_F_WRITE		2
+#define VIRTQ_DESC_F_INDIRECT	4
+struct virtq_descriptor {
+	le64 addr;
+	le32 len;
+	le16 flags;
+	le16 next;
+};
+
+// spec 2.6.6 The Virtqueue Available Ring https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-380006
+#define VIRTQ_AVAIL_F_NO_INTERRUPT	1
+struct virtq_available {
+	le16 flags;
+	le16 idx;
+	le16 *ring;
+	le16 used_event;
+};
+
+// spec 2.6.8 The Virtqueue Used Ring https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-430008
+#define VIRTQ_USED_F_NO_NOTIFY		1
+struct virtq_used_element {
+	le32 id;
+	le32 len;
+};
+
+struct virtq_used {
+	le16 flags;
+	le16 idx;
+	struct virtq_used_element *ring;
+	le16 available_event;
+};
+
+// spec 2.6.2 Legacy Interfaces: A Note on Virtqueue Layout https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-260002
+struct virtq {
+	uint num;
+	struct virtq_descriptor *desc;
+	struct virtq_available *avail;
+	struct virtq_used *used;
 };
 
 void virtio_init(struct virtio_device_info *dev);
