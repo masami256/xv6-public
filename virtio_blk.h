fbd74a683ff45111258a0fe32ad8e4b404f2939f
Masami Ichikawa <masami256@gmail.com>
2021-01-09 23:14:59 +0900

    virtio_blk: add virtio block device config structure

--- virtio_blk.h
+++ virtio_blk.h
@@ -1,6 +1,18 @@
 #ifndef _VIRTIO_BLK_H
 #define _VIRTIO_BLK_H
 
+#include "virtio.h"
+
+struct virtio_blk {
+	le64 total_sector_count;
+	le32 segment_size;
+	le32 segment_count;
+	le16 cylinder_count;
+	uchar head_count;
+	uchar sector_count;
+	uchar block_length;
+};
+
 void virtio_blk_init(void);
 
 #endif // _VIRTIO_BLK_H
