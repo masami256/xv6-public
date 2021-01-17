68e4745e3fda777903b79c21055ac0c88a9dc178
Masami Ichikawa <masami256@gmail.com>
2021-01-17 23:39:06 +0900

    virtio_blk: refactor virtio_blk.c

--- virtio_blk.c
+++ virtio_blk.c
@@ -23,6 +23,14 @@ static void virtio_read_block_device_info(struct virtio_device *vdev, struct vir
 	blk_info->head_count = inb(vdev->bar + VIRTIO_BLK_HEAD_COUNT);
 	blk_info->sector_count = inb(vdev->bar + VIRTIO_BLK_SECTOR_COUNT);
 	blk_info->block_length = inb(vdev->bar + VIRTIO_BLK_BLOCK_LENGTH);
+	
+	cprintf("[+]total sector: 0x%x\n", blk_info->total_sector_count);
+	cprintf("[+]max segment size: 0x%x\n", blk_info->segment_size);
+	cprintf("[+]max segment count: 0x%x\n", blk_info->segment_count);
+	cprintf("[+]cylinder count: 0x%x\n", blk_info->cylinder_count);
+	cprintf("[+]head count: 0x%x\n", blk_info->head_count);
+	cprintf("[+]sector count: 0x%x\n", blk_info->sector_count);
+	cprintf("[+]block length: 0x%x\n", blk_info->block_length);
 }
 
 static void virt_queue_init(struct virtio_device *vdev)
@@ -69,83 +77,74 @@ static void virt_queue_init(struct virtio_device *vdev)
 	q->avail->flags = 0;
 }
 
-static void virtio_init_driver(void)
+static void virtio_init_driver(struct virtio_device *vdev)
 {
 	uint device_features;
 	cprintf("[+]setup virtio block device\n");
 	uint status = 0;
 
-	vdev.pci_info.vendor = VIRTIO_VENDOR;
-	vdev.pci_info.device = VIRTIO_PCI_DEVICE_BLOCK_DEVICE;
-	vdev.pci_info.subsystem_vendor = VIRTIO_VENDOR;
-	vdev.pci_info.subsystem_id = VIRTIO_DEVICE_TYPE_BLOCK_DEVICE;
-	virtio_init(&vdev);
+	vdev->pci_info.vendor = VIRTIO_VENDOR;
+	vdev->pci_info.device = VIRTIO_PCI_DEVICE_BLOCK_DEVICE;
+	vdev->pci_info.subsystem_vendor = VIRTIO_VENDOR;
+	vdev->pci_info.subsystem_id = VIRTIO_DEVICE_TYPE_BLOCK_DEVICE;
+	virtio_init(vdev);
 
-	vdev.bar = get_device_base_address(&vdev);
-	cprintf("[+]I/O address space: 0x%x\n", vdev.bar);
+	vdev->bar = get_device_base_address(vdev);
+	cprintf("[+]I/O address space: 0x%x\n", vdev->bar);
 
 	cprintf("[+]found virtio block device\n");
 
-	cprintf("[+]initialize virtio driver(device:0x%x, subsystem:0x%x)\n", vdev.pci->device_id, vdev.pci->subsystem_id);
-	cprintf("[+]initial device status 0x%x\n", inb(vdev.bar + VIRTIO_CFG_OFFSET_DEVICE_STATUS));
+	cprintf("[+]initialize virtio driver(device:0x%x, subsystem:0x%x)\n", vdev->pci->device_id, vdev->pci->subsystem_id);
+	cprintf("[+]initial device status 0x%x\n", inb(vdev->bar + VIRTIO_CFG_OFFSET_DEVICE_STATUS));
 	cprintf("[+]reset the device\n");
 
 	status = VIRTIO_STATUS_DEVICE_NEEDS_RESET;
-	set_device_status(&vdev, status);
+	set_device_status(vdev, status);
 
 	// step1: Set ACKNOWLEDGE status bit
 	status = VIRTIO_STATUS_ACKNOWLEDGE;
-	set_device_status(&vdev, status);
+	set_device_status(vdev, status);
 
 	// step2: Set DRIVER status bit
 	status |= VIRTIO_STATUS_DRIVER;
-	set_device_status(&vdev, status);
+	set_device_status(vdev, status);
 
 	// step3: read device features bit
-	device_features = get_device_feature(&vdev);
+	device_features = get_device_feature(vdev);
 	cprintf("[+]device features: 0x%x\n", device_features);
 
-	set_driver_features(&vdev, device_features);
+	set_driver_features(vdev, device_features);
 
 	// step4 set FEATURES_OK status bia
 	status |= VIRTIO_STATUS_FEATURES_OK;
-	set_device_status(&vdev, status);
+	set_device_status(vdev, status);
 
 	// step5: read device status field and check FEATURES_OK bit still on
-	status = get_device_status(&vdev);
+	status = get_device_status(vdev);
 	if (!(status & VIRTIO_STATUS_FEATURES_OK))
 		panic("virtio unusable\n");
 
-	virt_queue_init(&vdev);
+	virt_queue_init(vdev);
 
 	// step6: setup queue address
-	set_queue_address(&vdev);
-	cprintf("[+]virtio queue physical address 0x%x\n", get_queue_size(&vdev));
+	set_queue_address(vdev);
+	cprintf("[+]virtio queue physical address 0x%x\n", get_queue_size(vdev));
 
 	// step7: write dirvers ok bit
 	status |= VIRTIO_STATUS_DRIVER_OK;
-	set_device_status(&vdev, status);
+	set_device_status(vdev, status);
 
-	status = get_device_status(&vdev);
+	status = get_device_status(vdev);
 	if ((status == VIRTIO_STATUS_FAILD) || !(status & VIRTIO_STATUS_FEATURES_OK))
 		panic("failed to setup virtio driver\n");
 
 	cprintf("[+]final device status: 0x%x\n", status);
 	cprintf("[+]virtio driver initialize done.\n");
-
-
 }
 
 void virtio_blk_init(void)
 {
-	virtio_init_driver();
+	virtio_init_driver(&vdev);
 	virtio_read_block_device_info(&vdev, &blk_info);
 
-	cprintf("total sector: 0x%x\n", blk_info.total_sector_count);
-	cprintf("max segment size: 0x%x\n", blk_info.segment_size);
-	cprintf("max segment count: 0x%x\n", blk_info.segment_count);
-	cprintf("cylinder count: 0x%x\n", blk_info.cylinder_count);
-	cprintf("head count: 0x%x\n", blk_info.head_count);
-	cprintf("sector count: 0x%x\n", blk_info.sector_count);
-	cprintf("block length: 0x%x\n", blk_info.block_length);
 }
